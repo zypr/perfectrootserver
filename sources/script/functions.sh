8589e358feaa284e015a102c2869ff1834145f0a
shoujii <retender.jw@gmail.com>
2016-09-03 17:56:38 +0200

    Update functions.sh

--- sources/script/functions.sh
+++ sources/script/functions.sh
@@ -1787,7 +1787,7 @@ if [ ${USE_AJENTI} == '1' ] && [ ${USE_VALID_SSL} == '1' ]; then
 	sed -i.bak 's/^[[:space:]]*"password.*$/"password" : "sha512|'"${ajentihash//\//\\/}"'",/' /etc/ajenti/config.json
 	service ajenti restart
 	
-	$AJENTI_PORTS="8000"
+	AJENTI_PORTS="8000"
 	sed -i "/^OPEN_TCP=\"/ s//&$AJENTI_PORTS,/" /etc/arno-iptables-firewall/firewall.conf >/dev/null 2>&1
 	
 	#AJENTI UDP credentials.txt All Ports are open output is wrong
@@ -1814,38 +1814,38 @@ if [ ${USE_TEAMSPEAK} == '1' ]; then
 	timeout 10 sudo -u  ts3user /usr/local/ts3user/ts3server/ts3server_minimal_runscript.sh > ts3serverdata.txt
 
 
-	echo "#! /bin/sh
-	### BEGIN INIT INFO
-	# Provides:         ts3server
-	# Required-Start: 	"'$local_fs $network'"
-	# Required-Stop:	"'$local_fs $network'"
-	# Default-Start: 	2 3 4 5
-	# Default-Stop: 	0 1 6
-	# Description:      TS 3 Server
-	### END INIT INFO
+echo "#! /bin/sh
+### BEGIN INIT INFO
+# Provides:         ts3server
+# Required-Start: 	"'$local_fs $network'"
+# Required-Stop:	"'$local_fs $network'"
+# Default-Start: 	2 3 4 5
+# Default-Stop: 	0 1 6
+# Description:      TS 3 Server
+### END INIT INFO
 
-	case "'"$1"'" in
-	start)
-	echo "'"Starte Teamspeak 3 Server ... "'"
-	su ts3user -c "'"/usr/local/ts3user/ts3server/ts3server_startscript.sh start"'"
-	;;
-	stop)
-	echo "'"Beende Teamspeak 3 Server ..."'"
-	su ts3user -c "'"/usr/local/ts3user/ts3server/ts3server_startscript.sh stop"'"
-	;;
-	*)
-	echo "'"Sie können folgende Befehle nutzen: TS3 starten: /etc/init.d/ts3server start TS3 stoppen: /etc/init.d/ts3server stop"'" > /usr/local/ts3user/ts3server/ts3befehle.txt
-	exit 1
-	;;
-	esac
-	exit 0" >> /etc/init.d/ts3server 
+case "'"$1"'" in
+start)
+echo "'"Starte Teamspeak 3 Server ... "'"
+su ts3user -c "'"/usr/local/ts3user/ts3server/ts3server_startscript.sh start"'"
+;;
+stop)
+echo "'"Beende Teamspeak 3 Server ..."'"
+su ts3user -c "'"/usr/local/ts3user/ts3server/ts3server_startscript.sh stop"'"
+;;
+*)
+echo "'"Sie können folgende Befehle nutzen: TS3 starten: /etc/init.d/ts3server start TS3 stoppen: /etc/init.d/ts3server stop"'" > /usr/local/ts3user/ts3server/ts3befehle.txt
+exit 1
+;;
+esac
+exit 0" >> /etc/init.d/ts3server 
 
 	chmod 755 /etc/init.d/ts3server
 	update-rc.d ts3server defaults
 	/etc/init.d/ts3server start >/dev/null 2>&1
 	
-	$TS3_PORTS_TCP="2010, 9987, "
-	$TS3_PORTS_UDP="2008, 10011, 30033, 41144, "
+	TS3_PORTS_TCP="2010, 9987, "
+	TS3_PORTS_UDP="2008, 10011, 30033, 41144, "
 	sed -i "/^OPEN_TCP=\"/ s//&$TS3_PORTS_TCP,/" /etc/arno-iptables-firewall/firewall.conf >/dev/null 2>&1
 	sed -i "/^OPEN_UDP=\"/ s//&$TS3_PORTS_UDP,/" /etc/arno-iptables-firewall/firewall.conf >/dev/null 2>&1
 fi
