d43e998b0066746d4dc84f6580407628ac75a68b
shoujii <retender.jw@gmail.com>
2016-09-04 23:19:11 +0200

    Update vsftpd.sh

--- sources/script/vsftpd.sh
+++ sources/script/vsftpd.sh
@@ -145,4 +145,8 @@ Your password: $userpass
 -------------------------------------------------------
 END
 sed -i "/^OPEN_TCP=\"/ s//&$FTP_PORT,/" /etc/arno-iptables-firewall/firewall.conf >/dev/null 2>&1
+
+	cat /root/VSFTP_LOGINDATA.txt >> ~/credentials.txt
+	echo "" >> ~/credentials.txt
+	echo "" >> ~/credentials.txt
 fi
