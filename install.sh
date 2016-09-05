a126e626591930053d82f1690270175d4ec77b3b
shoujii <retender.jw@gmail.com>
2016-09-05 21:56:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,22 @@
 
 source sources/script/functions.sh
 
+source sources/script/AddNewSite.sh
+source sources/script/ajenti.sh
+source sources/script/openvpn.sh
+source sources/script/ts3.sh
+source sources/script/vsftpd.sh
+
+
 checksystem
 checkconfig
 installation
+
+addnewsite
+ajenti
+openvpn
+ts3
+vsftpd
+
 logininformation
 instructions
