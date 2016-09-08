2928c9c75d6cb80d70fc533f359d647ec469330b
BoBBer446 <eXestend@gmx.de>
2016-09-08 15:09:55 +0200

    Update addnewsite.sh
    Att some fixes

--- sources/script/addnewsite.sh
+++ sources/script/addnewsite.sh
@@ -22,7 +22,7 @@ mkdir -p /etc/nginx/html/${MYOTHERDOMAIN}/htdocs/
 chown -R www-data:www-data /etc/nginx/html/${MYOTHERDOMAIN}/
 chmod 755 /etc/nginx/html/
 
-
+#Stop System
 systemctl stop nginx.service
 
 # SSL certificate
@@ -34,7 +34,7 @@ if [ ${CLOUDFLARE} == '0' ] && [ ${USE_VALID_SSL} == '1' ]; then
 	#if [ ${USE_MAILSERVER} == '1' ]; then
 		#./letsencrypt-auto --agree-tos --renew-by-default --non-interactive --standalone --email ${SSLMAIL} --rsa-key-size 2048 -d ${MYOTHERDOMAIN} -d www.${MYOTHERDOMAIN} -d mail.${MYOTHERDOMAIN} -d autodiscover.${MYOTHERDOMAIN} -d autoconfig.${MYOTHERDOMAIN} -d dav.${MYOTHERDOMAIN} certonly >/dev/null 2>&1
 	#else
-		./letsencrypt-auto --agree-tos --renew-by-default --non-interactive --standalone --email ${SSLMAIL} --rsa-key-size 2048 -d ${MYOTHERDOMAIN} -d www.${MYOTHERDOMAIN} certonly >/dev/null 2>&1
+	./letsencrypt-auto --agree-tos --renew-by-default --non-interactive --standalone --email ${SSLMAIL} --rsa-key-size 2048 -d ${MYOTHERDOMAIN} -d www.${MYOTHERDOMAIN} certonly >/dev/null 2>&1
 	#fi
 	ln -s /etc/letsencrypt/live/${MYOTHERDOMAIN}/fullchain.pem /etc/nginx/ssl/${MYOTHERDOMAIN}.pem
 	ln -s /etc/letsencrypt/live/${MYOTHERDOMAIN}/privkey.pem /etc/nginx/ssl/${MYOTHERDOMAIN}.key.pem
@@ -54,9 +54,11 @@ openssl dhparam -out /etc/nginx/ssl/dh.pem 2048 >/dev/null 2>&1
 #This works only in installation script, do not use for updatescript
 #In Update script hte user havte to type the domain.tld in config part
 #Disable default_server in first domain if this script add another one
-sed 's/80 default_server;/80;/g'  /etc/nginx/sites-available/${MYDOMAIN}.conf
-sed 's/server_name 		${IPADR} /server_name 		;/g'  /etc/nginx/sites-available/${MYDOMAIN}.conf
-sed 's/ deferred;/;/g'  /etc/nginx/sites-available/${MYDOMAIN}.conf
+
+#The new Conf dontneed that
+#sed 's/80 default_server;/80;/g'  /etc/nginx/sites-available/${MYDOMAIN}.conf
+#sed 's/server_name 		${IPADR} /server_name 		;/g'  /etc/nginx/sites-available/${MYDOMAIN}.conf
+#sed 's/ deferred;/;/g'  /etc/nginx/sites-available/${MYDOMAIN}.conf
 
 
 # Create server config
@@ -86,7 +88,7 @@ server {
 
 			ssl_certificate 	ssl/${MYOTHERDOMAIN}.pem;
 			ssl_certificate_key ssl/${MYOTHERDOMAIN}.key.pem;
-			#ssl_trusted_certificate ssl/${MYOTHERDOMAIN}.pem;
+			ssl_trusted_certificate ssl/${MYOTHERDOMAIN}.pem;
 			ssl_dhparam	     	ssl/dh.pem;
 			ssl_ecdh_curve		secp384r1;
 			ssl_session_cache   shared:SSL:10m;
@@ -96,15 +98,15 @@ server {
 			ssl_prefer_server_ciphers on;
 			ssl_buffer_size 	1400;
 
-			#ssl_stapling 		on;
-			#ssl_stapling_verify on;
-			#resolver 			8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220 valid=60s;
-			#resolver_timeout 	2s;
+			ssl_stapling 		on;
+			ssl_stapling_verify on;
+			resolver 			8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220 valid=60s;
+			resolver_timeout 	2s;
 
 			ssl_ciphers 		"ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK";
 
-			#add_header 		Strict-Transport-Security "max-age=63072000; includeSubdomains; preload";
-			##add_header 		Public-Key-Pins 'pin-sha256="PIN1"; pin-sha256="PIN2"; max-age=5184000; includeSubDomains';
+			add_header 		Strict-Transport-Security "max-age=63072000; includeSubdomains; preload";
+			#add_header 		Public-Key-Pins 'pin-sha256="PIN1"; pin-sha256="PIN2"; max-age=5184000; includeSubDomains';
 			add_header 			Cache-Control "public";
 			add_header 			X-Frame-Options SAMEORIGIN;
 			add_header 			Alternate-Protocol  443:npn-http/2;
@@ -115,7 +117,7 @@ server {
 			add_header 			"Access-Control-Allow-Origin" "*";
 			add_header 			Content-Security-Policy "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.youtube.com maps.gstatic.com *.googleapis.com *.google-analytics.com cdnjs.cloudflare.com assets.zendesk.com connect.facebook.net; frame-src 'self' *.youtube.com assets.zendesk.com *.facebook.com s-static.ak.facebook.com tautt.zendesk.com; object-src 'self'";
 
-			pagespeed 			on;
+			pagespeed 			off;
 			pagespeed 			EnableFilters collapse_whitespace;
 			pagespeed 			EnableFilters canonicalize_javascript_libraries;
 			pagespeed 			EnableFilters combine_css;
@@ -157,14 +159,14 @@ server {
 
 			auth_basic_user_file htpasswd/.htpasswd;
 
-			location ~ \.php\$ {
-				fastcgi_split_path_info ^(.+\.php)(/.+)\$;
-				if (!-e \$document_root\$fastcgi_script_name) {
+			location ~ \.php$ {
+				fastcgi_split_path_info ^(.+\.php)(/.+)$;
+				if (!-e $document_root$fastcgi_script_name) {
 					return 404;
 			  	}
-				try_files \$fastcgi_script_name =404;
-				fastcgi_param PATH_INFO \$fastcgi_path_info;
-				fastcgi_param PATH_TRANSLATED \$document_root\$fastcgi_path_info;
+				try_files $fastcgi_script_name =404;
+				fastcgi_param PATH_INFO $fastcgi_path_info;
+				fastcgi_param PATH_TRANSLATED $document_root$fastcgi_path_info;
 				fastcgi_param APP_ENV production;
 				fastcgi_pass unix:/var/run/php5-fpm.sock;
 				fastcgi_index index.php;
@@ -187,7 +189,7 @@ server {
 				# URL. Usefull if you use Codeigniter, Zendframework, etc.
 				# or just need to remove the index.php
 				#
-			   	#try_files \$uri \$uri/ /index.php?\$args;
+			   	#try_files $uri $uri/ /index.php?$args;
 			}
 
 			location ~* /\.(?!well-known\/) {
@@ -213,8 +215,8 @@ server {
 				log_not_found off;
 			}
 
-			location ~* ^.+\.(css|js)\$ {
-				rewrite ^(.+)\.(\d+)\.(css|js)\$ \$1.\$3 last;
+			location ~* ^.+\.(css|js|jsonp)$ {
+				rewrite ^(.+)\.(\d+)\.(css|js)$ $1.$3 last;
 				expires 30d;
 				access_log off;
 				log_not_found off;
@@ -222,7 +224,7 @@ server {
 				add_header Cache-Control "max-age=2592000, public";
 			}
 
-			location ~* \.(asf|asx|wax|wmv|wmx|avi|bmp|class|divx|doc|docx|eot|exe|gif|gz|gzip|ico|jpg|jpeg|jpe|mdb|mid|midi|mov|qt|mp3|m4a|mp4|m4v|mpeg|mpg|mpe|mpp|odb|odc|odf|odg|odp|ods|odt|ogg|ogv|otf|pdf|png|pot|pps|ppt|pptx|ra|ram|svg|svgz|swf|tar|t?gz|tif|tiff|ttf|wav|webm|wma|woff|wri|xla|xls|xlsx|xlt|xlw|zip)\$ {
+			location ~* \.(asf|asx|wax|wmv|wmx|avi|bmp|class|divx|doc|docx|eot|exe|gif|gz|gzip|ico|jpg|jpeg|jpe|mdb|mid|midi|mov|qt|mp3|m4a|mp4|m4v|mpeg|mpg|mpe|mpp|odb|odc|odf|odg|odp|ods|odt|ogg|ogv|otf|pdf|png|pot|pps|ppt|pptx|ra|ram|svg|svgz|swf|tar|t?gz|tif|tiff|ttf|wav|webm|wma|woff|wri|xla|xls|xlsx|xlt|xlw|zip)$ {
 				expires 30d;
 				access_log off;
 				log_not_found off;
@@ -236,13 +238,15 @@ server {
 }
 END
 
+#Activate the new site
 ln -s /etc/nginx/sites-available/${MYOTHERDOMAIN}.conf /etc/nginx/sites-enabled/${MYOTHERDOMAIN}.conf
 
-if [ ${CLOUDFLARE} == '0' ] && [ ${USE_VALID_SSL} == '1' ]; then
-	sed -i 's/#ssl/ssl/g' /etc/nginx/sites-available/${MYOTHERDOMAIN}.conf
-	sed -i 's/#resolver/resolver/g' /etc/nginx/sites-available/${MYOTHERDOMAIN}.conf
-	sed -i 's/#add/add/g' /etc/nginx/sites-available/${MYOTHERDOMAIN}.conf
-fi
+#The new conf dont need that
+#if [ ${CLOUDFLARE} == '0' ] && [ ${USE_VALID_SSL} == '1' ]; then
+#	sed -i 's/#ssl/ssl/g' /etc/nginx/sites-available/${MYOTHERDOMAIN}.conf
+#	sed -i 's/#resolver/resolver/g' /etc/nginx/sites-available/${MYOTHERDOMAIN}.conf
+#	sed -i 's/#add/add/g' /etc/nginx/sites-available/${MYOTHERDOMAIN}.conf
+#fi
 
 
 #Create index.html
@@ -253,6 +257,7 @@ cat > /etc/nginx/html/${MYOTHERDOMAIN}/htdocs/index.html <<END
   </head>
   <body>
     <h1>Success: You Have Set Up a Virtual Host</h1>
+    <p>If you have any question, please visit the offical site <a href="https://perfectrootserver.de">perfectrootserver.de</a></p>
   </body>
 </html>
 END
