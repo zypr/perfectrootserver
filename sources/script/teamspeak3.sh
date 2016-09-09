d793a6c79b998db7287c702cf143a40c5544d573
shoujii <retender.jw@gmail.com>
2016-09-09 19:34:11 +0200

    Update and rename ts3.sh to teamspeak3.sh

--- sources/script/teamspeak3.sh
+++ sources/script/teamspeak3.sh
@@ -4,7 +4,7 @@
 # Big thanks to https://github.com/andryyy/mailcow and https://github.com/zypr/perfectrootserver
 # Compatible with Debian 8.x (jessie)
 
-ts3() {
+teamspeak3() {
 
 source ~/addonconfig.cfg
 
