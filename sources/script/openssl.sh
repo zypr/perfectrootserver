864bf9ce06e5a004e70990a40780fe7b359c057c
shoujii <retender.jw@gmail.com>
2016-09-24 20:26:02 +0200

    Update openssl.sh

--- sources/script/openssl.sh
+++ sources/script/openssl.sh
@@ -80,5 +80,23 @@ cat > $_ <<END
 END
 systemctl -q restart ssh.service
 
+# Public Key Authentication
+echo "${info} Generating key for public key authentication..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
+ssh-keygen -f ~/ssh.key -b 3072 -t rsa -N ${SSH_PASS} >/dev/null 2>&1
+mkdir -p ~/.ssh && chmod 700 ~/.ssh
+cat ~/ssh.key.pub > ~/.ssh/authorized_keys2 && rm ~/ssh.key.pub
+chmod 600 ~/.ssh/authorized_keys2
+mv ~/ssh.key ~/ssh_privatekey.txt
+
+sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/^UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+echo -e "" >> /etc/ssh/sshd_config
+echo -e "# Disable password based logins" >> /etc/ssh/sshd_config
+echo -e "AuthenticationMethods publickey" >> /etc/ssh/sshd_config
+systemctl -q restart ssh.service
+
+truncate -s 0 /var/log/daemon.log
+truncate -s 0 /var/log/syslog
+
 }
 source ~/userconfig.cfg
