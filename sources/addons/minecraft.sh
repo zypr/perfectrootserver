efe0e488bfd5bdc89d1880e9607642ebd27a953d
shoujii <retender.jw@gmail.com>
2016-10-02 20:02:13 +0200

    Update minecraft.sh

--- sources/addons/minecraft.sh
+++ sources/addons/minecraft.sh
@@ -16,10 +16,8 @@ apt-get update -y >/dev/null 2>&1 && apt-get -y upgrade >/dev/null 2>&1
 apt-get -y install screen >/dev/null 2>&1
 apt-get -y install openjdk-7-jre-headless >/dev/null 2>&1
 
-echo "${info} Creating User..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
 adduser minecraft --gecos "" --no-create-home --disabled-password >/dev/null 2>&1
 
-echo "${info} Add Firewall Ports..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
 MINECRAFT_PORTS="25565"
        sed -i "/\<$MINECRAFT_PORTS\>/ "\!"s/^OPEN_TCP=\"/&$MINECRAFT_PORTS, /" /etc/arno-iptables-firewall/firewall.conf
 
@@ -28,13 +26,11 @@ sleep 1
 #If the Addon runs in Standalone we need that
 systemctl force-reload arno-iptables-firewall.service >/dev/null 2>&1
 
-echo "${info} Download..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
 mkdir /usr/local/minecraft/
 chown minecraft /usr/local/minecraft/
 cd /usr/local/minecraft/
 sudo -u  minecraft wget -q https://s3.amazonaws.com/Minecraft.Download/versions/${MINECRAFT_VERSION}/minecraft_server.${MINECRAFT_VERSION}.jar
 
-echo "${info} Create Config..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
 echo "#!/bin/bash
 cd /usr/local/minecraft/
 java -Xmx1024M -Xms1024M -jar minecraft_server.*.*.*.jar nogui
