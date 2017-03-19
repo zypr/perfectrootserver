64b1a351d3ac314e9c339141d3548c152f5e86d1
shoujii <retender.jw@gmail.com>
2017-03-19 21:37:15 +0100

    Delete old structure

--- assistant.sh
+++
@@ -1,9 +0,0 @@
-# The perfect rootserver
-# by zypr
-# https://github.com/zypr/perfectrootserver
-# Big thanks to https://github.com/andryyy/mailcow
-# Compatible with Debian 8.x (jessie)
-
-source sources/script/instructions.sh
-
-instructions
