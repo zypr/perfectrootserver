7d43c2ae7e970c5a0dcc2e122addbacde18fb9be
BoBBer446 <eXestend@gmx.de>
2017-04-11 07:08:21 +0200

    fix header

--- addons/backup.sh
+++ addons/backup.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-# The perfect rootserver
-# by shoujii | BoBBer446
-# This is an standalone addonscript for 
+# The perfect rootserver - Your Webserverinstallation Script!
+# by shoujii | BoBBer446 > 2017
+#####
 # https://github.com/shoujii/perfectrootserver
+# Compatible with Debian 8.x (jessie)
+# Special thanks to Zypr!
+#
+	# This program is free software; you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation; either version 2 of the License, or
+    # (at your option) any later version.
 
-# Enable Debug
-#set -x
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License along
+    # with this program; if not, write to the Free Software Foundation, Inc.,
+    # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#-------------------------------------------------------------------------------------------------------------
 
 ##########################################################################
 ###################### DO NOT EDIT ANYTHING BELOW! #######################
@@ -132,7 +146,7 @@ cd /root
 #find $BACKUP_PATH/* -mtime +30 -exec rm {} \;
 
 
-#Backup, in die install einfügen
+#Backup, in die install einfÃ¼gen
 # http://www.sysadminslife.com/linux/cronjob-unter-debian-und-ubuntu-erstellen/
 #crontab -e
 #00 20 * * * /path/to/backup.sh
