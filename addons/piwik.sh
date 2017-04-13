02f989a214dd5223a361604fc63c4a8d94142cd4
BoBBer446 <eXestend@gmx.de>
2017-04-13 18:41:42 +0200

    Fix nginx site conf

--- addons/piwik.sh
+++ addons/piwik.sh
@@ -20,18 +20,35 @@
     # with this program; if not, write to the Free Software Foundation, Inc.,
     # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #-------------------------------------------------------------------------------------------------------------
-
-#################################
-##  DO NOT MODIFY, JUST DON'T! ##
-#################################
+################################################################
+################## ATTENTION ! NOT UP TO DATE ##################
+################## ATTENTION ! NOT UP TO DATE ##################
+############################ 04.2017 ###########################
+################################################################
+# >>> -.. ---     -. --- -     ..- ... .     .. -     -·-·--<<< #
+#----------------------------------------------------------------------#
+#-------------------DO NOT EDIT SOMETHING BELOW THIS-------------------#
+#----------------------------------------------------------------------#
 
 piwikinstall() {
+echo "${info} Installing Prestashop" | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
+
+if [ ${USE_PIWIK} == '1' ]; then
+
+# Check if Perfectrootserver Script is installed
+if [ ! -f /root/credentials.txt ]; then
+    echo "${error} Can not find file /root/credentials.txt!" | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
+	exit 0
+fi
+
+touch  /root/logs/stderrorPIWIK.log 
+touch /root/logs/stdoutPIWIK.log
 
 
-cd /usr/local/
-wget https://builds.piwik.org/piwik.zip
-unzip piwik.zip
-rm -rf piwik.zip
+cd /usr/local/ >>/root/logs/stderrorPIWIK.log 2>&1 >>/root/logs/stdoutPIWIK.log
+wget https://builds.piwik.org/piwik.zip >>/root/logs/stderrorPIWIK.log 2>&1 >>/root/logs/stdoutPIWIK.log
+unzip piwik.zip >>/root/logs/stderrorPIWIK.log 2>&1 >>/root/logs/stdoutPIWIK.log
+rm -rf piwik.zip >>/root/logs/stderrorPIWIK.log 2>&1 >>/root/logs/stdoutPIWIK.log
 
 cat > /etc/nginx/sites-custom/piwik.conf <<END
 location /piwik {
@@ -39,6 +56,10 @@ location /piwik {
 	#Installationspfad
     alias /usr/local/piwik;
     index index.php;
+	
+	#allow   127.0.0.1;
+	#allow   127.0.0.1;
+	#deny    all;
 
     location ~ ^/piwik/(.+\.php)$ {
         alias /usr/local/piwik/$1;
@@ -48,9 +69,28 @@ location /piwik {
         fastcgi_param SCRIPT_FILENAME /usr/local/piwik/$1;
         fastcgi_pass unix:/var/run/php5-fpm.sock;
     }
+
+    location ~* ^/piwik/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt))$ {
+        alias /usr/local/piwik/$1;
+    }
+
+    location ~ ^/piwik/save/ {
+        deny all;
+    }
+
+    location ~ ^/piwik/upload/ {
+        deny all;
+    }
 }
 
-END
+if [ ${USE_PHP7} == '1' ] && [ ${USE_PHP5} == '0' ]; then
+
+	sed -i 's/fastcgi_pass unix:\/var\/run\/php5-fpm.sock\;/fastcgi_pass unix:\/var\/run\/php\/php7.0-fpm.sock\;/g' /etc/nginx/sites-available/${MYDOMAIN}.conf
+
+	#fastcgi_pass unix:\/var\/run\/php5-fpm.sock;
+
+	#fastcgi_pass unix:\/var\/run\/php\/php7.0-fpm.sock;
+fi
 
 mkdir -p /usr/local/piwik/tmp/assets/
 mkdir -p /usr/local/piwik/tmp/cache/
@@ -67,10 +107,21 @@ chmod -R 0755 /usr/local/piwik/tmp/tcpdf/
 chmod -R 0755 /usr/local/piwik/tmp/templates_c/
 
 
-sed 's/;always_populate_raw_post_data = -1/always_populate_raw_post_data = -1/' /etc/php5/fpm/php.ini
-
-service nginx restart
-service php5-fpm restart
+sed 's/;always_populate_raw_post_data = -1/always_populate_raw_post_data = -1/' /etc/php5/fpm/php.ini >>/root/logs/stderrorPIWIK.log 2>&1 >>/root/logs/stdoutPIWIK.log
 
 
+
+#Restarting services
+if [ ${USE_PHP7} == '1' ]; then
+		systemctl restart {nginx,php7.0-fpm} >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+fi
+
+if [ ${USE_PHP5} == '1' ]; then
+		systemctl restart {nginx,php5-fpm} >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+fi
+
+
+fi # End var USE_PIWIK
 }
+source ~/configs/userconfig.cfg
+source ~/configs/addonconfig.cfg
