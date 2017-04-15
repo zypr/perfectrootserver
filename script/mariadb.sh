59f2c731ecc1096db719cbd814f3a1224ded41a3
shoujii <retender.jw@gmail.com>
2017-04-16 00:43:14 +0200

    Update mariadb.sh

--- script/mariadb.sh
+++ script/mariadb.sh
@@ -28,7 +28,7 @@
 mariadb() {
 
 echo "${info} Installing MariaDB..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
-apt-get -qq update && apt-get -q -y --force-yes install mariadb-server >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+apt-get -qq update && apt-get -q -y --force-yes install mariadb-server ${log}
 
 mysqladmin -u root password ${MYSQL_ROOT_PASS}
 
@@ -45,6 +45,6 @@ sed -i '40s/.*/innodb_log_buffer_size = 16M\n&/' /etc/mysql/my.cnf
 sed -i '41s/.*/#innodb_table_locks = 0 #disable table lock, uncomment if you do not want to crash all applications, if one does\n&/' /etc/mysql/my.cnf
 
 # Automated mysql_secure_installation
-mysql -u root -p${MYSQL_ROOT_PASS} -e "DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; FLUSH PRIVILEGES; DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; FLUSH PRIVILEGES;" >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+mysql -u root -p${MYSQL_ROOT_PASS} -e "DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; FLUSH PRIVILEGES; DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; FLUSH PRIVILEGES;" ${log}
 }
 source ~/configs/userconfig.cfg
