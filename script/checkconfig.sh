f49f3f46bebafae74b586aa4861ac60a1e7cec74
shoujii <retender.jw@gmail.com>
2017-04-16 00:29:17 +0200

    Update checkconfig.sh

--- script/checkconfig.sh
+++ script/checkconfig.sh
@@ -28,7 +28,7 @@
 checkconfig() {
 
 	echo "${info} Checking your configuration..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
-	for var in TIMEZONE MYDOMAIN SSH_PORT SSH_PASS USE_VALID_SSL SSLMAIL USE_MAILSERVER USE_PHP5 USE_PHP7 USE_WEBMAIL POSTFIX_ADMIN_PASS VIMB_MYSQL_PASS ROUNDCUBE_MYSQL_PASS USE_PMA PMA_HTTPAUTH_USER PMA_HTTPAUTH_PASS PMA_BFSECURE_PASS PMA_RESTRICT MYSQL_ROOT_PASS MYSQL_PMADB_NAME MYSQL_PMADB_USER MYSQL_PMADB_PASS MYSQL_HOSTNAME CLOUDFLARE SET_UP_RSA_KEY ALLOWHTTPCONNECTIONS DEBUG_IS_SET                 
+	for var in TIMEZONE MYDOMAIN SSH_PORT SSH_PASS USE_VALID_SSL SSLMAIL USE_MAILSERVER USE_PHP5 USE_PHP7 USE_WEBMAIL POSTFIX_ADMIN_PASS VIMB_MYSQL_PASS ROUNDCUBE_MYSQL_PASS USE_PMA PMA_HTTPAUTH_USER PMA_HTTPAUTH_PASS PMA_BFSECURE_PASS PMA_RESTRICT MYSQL_ROOT_PASS MYSQL_PMADB_NAME MYSQL_PMADB_USER MYSQL_PMADB_PASS MYSQL_HOSTNAME CLOUDFLARE HIGH_SECURITY ALLOWHTTPCONNECTIONS DEBUG_IS_SET                 
 	do
 		if [[ -z ${!var} ]]; then
 			echo "${error} Parameter $(textb ${var}) must not be empty." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
@@ -47,14 +47,14 @@ checkconfig() {
 		fi
 
 	if [ $(dpkg-query -l | grep libcrack2 | wc -l) -ne 1 ]; then
-		apt-get update -y >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log && apt-get -y --force-yes install libcrack2 >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+		apt-get update -y ${log} && apt-get -y --force-yes install libcrack2 ${log}
 	fi
 
 	for var in ${SSH_PASS} ${POSTFIX_ADMIN_PASS} ${VIMB_MYSQL_PASS} ${ROUNDCUBE_MYSQL_PASS} ${PMA_HTTPAUTH_PASS} ${PMA_BFSECURE_PASS} ${MYSQL_ROOT_PASS} ${MYSQL_PMADB_PASS}
 	do
-		if echo "${var}" | grep -P '(?=^.{8,255}$)(?=^[^\s]*$)(?=.*\d)(?=.*[A-Z])(?=.*[a-z])' >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log; then
+		if echo "${var}" | grep -P '(?=^.{8,255}$)(?=^[^\s]*$)(?=.*\d)(?=.*[A-Z])(?=.*[a-z])' ${log}; then
 			if [[ "$(awk -F': ' '{ print $2}' <<<"$(cracklib-check <<<"${var}")")" == "OK" ]]; then
-				echo >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+				echo ${log}
 			else
 				echo "${error} One of your passwords was rejected!" | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
 				echo "${info} Your password must be a minimum of 8 characters and must include at least 1 number, 1 uppercase and 1 lowercase letter." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
@@ -96,6 +96,11 @@ checkconfig() {
 		
 		if [ ${USE_PHP5} == '0' ] && [ ${USE_PHP7} == '0' ]; then
 		echo "${error} You have to choose a PHP Version" | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
+		exit 1
+		fi
+		
+		if [ ${ALLOWHTTPCONNECTIONS} == '1' ] && [ ${HIGH_SECURITY} == '1' ]; then
+		echo "${error} You can not use ALLOWHTTPCONNECTIONS and HIGH_SECURITY together, please set one option to 0" | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
 		exit 1
 		fi
 
