9e0b8b19d7880990b68389c6920fb6dd6cc60c04
shoujii <retender.jw@gmail.com>
2017-04-16 00:44:30 +0200

    Update nginx.sh

--- script/nginx.sh
+++ script/nginx.sh
@@ -30,26 +30,26 @@ nginx() {
 # Nginx
 cd ~/sources
 echo "${info} Downloading Nginx Pagespeed..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
-wget https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}-beta.zip >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-unzip v${NPS_VERSION}-beta.zip >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+wget https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}-beta.zip ${log}
+unzip v${NPS_VERSION}-beta.zip ${log}
 
 echo "${info} Downloading Nginx Pagespeed psol..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
-cd ngx_pagespeed-${NPS_VERSION}-beta/ >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+cd ngx_pagespeed-${NPS_VERSION}-beta/ ${log}
 
 #Todo: fix choice
 
 	#x64
-	wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}-x64.tar.gz >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-	tar -xzf ${NPS_VERSION}-x64.tar.gz >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+	wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}-x64.tar.gz ${log}
+	tar -xzf ${NPS_VERSION}-x64.tar.gz ${log}
 
 	#x32
-	#wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}-ia32.tar.gz >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-	#tar -xzf ${NPS_VERSION}-ia32.tar.gz >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+	#wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}-ia32.tar.gz ${log}
+	#tar -xzf ${NPS_VERSION}-ia32.tar.gz ${log}
 
 
 cd ~/sources
 echo "${info} Downloading Nginx..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
-wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz ${log}
 tar -xzf nginx-${NGINX_VERSION}.tar.gz
 cd nginx-${NGINX_VERSION}
 
@@ -95,7 +95,7 @@ echo "${info} Compiling Nginx..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
 --with-pcre \
 --with-cc-opt='-O2 -g -pipe -Wall -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic' \
 --with-openssl=$HOME/sources/openssl-${OPENSSL_VERSION} \
---add-module=$HOME/sources/ngx_pagespeed-${NPS_VERSION}-beta >>/root/logs/make.log 2>&1
+--add-module=$HOME/sources/ngx_pagespeed-${NPS_VERSION}-beta ${makelog}
 
 #habe "--with-debug \" erstmal raus genommen. Gibt Probleme
 #
@@ -108,12 +108,12 @@ echo "${info} Compiling Nginx..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
 #
 # ToDo: Autimatic installer maybe....
 # make the package
-make >>/root/logs/make.log 2>&1
+make ${makelog}
 # Create a .deb package
-checkinstall --install=no -y >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+checkinstall --install=no -y ${log}
 # Install the package
 echo "${info} Installing Nginx..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
-dpkg -i nginx_${NGINX_VERSION}-1_amd64.deb >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+dpkg -i nginx_${NGINX_VERSION}-1_amd64.deb ${log}
 mv nginx_${NGINX_VERSION}-1_amd64.deb ../
 
 # Create directories
@@ -135,7 +135,7 @@ chown -R www-data:www-data /var/lib/nginx
 chown www-data:www-data /etc/nginx/logs
 
 # Install the Nginx service script
-wget -O /etc/init.d/nginx --no-check-certificate https://raw.githubusercontent.com/Fleshgrinder/nginx-sysvinit-script/master/init >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+wget -O /etc/init.d/nginx --no-check-certificate https://raw.githubusercontent.com/Fleshgrinder/nginx-sysvinit-script/master/init ${log}
 chmod 0755 /etc/init.d/nginx
 chown root:root /etc/init.d/nginx
 update-rc.d nginx defaults
@@ -215,24 +215,24 @@ if [ ${CLOUDFLARE} == '0' ] && [ ${USE_VALID_SSL} == '1' ]; then
 	git clone https://github.com/letsencrypt/letsencrypt ~/sources/letsencrypt -q
 	cd ~/sources/letsencrypt
 	if [ ${USE_MAILSERVER} == '1' ]; then
-		./letsencrypt-auto --agree-tos --renew-by-default --non-interactive --standalone --email ${SSLMAIL} --rsa-key-size ${RSA_KEY_SIZE} -d ${MYDOMAIN} -d www.${MYDOMAIN} -d mail.${MYDOMAIN} certonly >>/root/stderror.log 2>&1  >> /root/stdout.log
+		./letsencrypt-auto --agree-tos --renew-by-default --non-interactive --standalone --email ${SSLMAIL} --rsa-key-size ${RSA_KEY_SIZE} -d ${MYDOMAIN} -d www.${MYDOMAIN} -d mail.${MYDOMAIN} certonly ${log}
 	else
-		./letsencrypt-auto --agree-tos --renew-by-default --non-interactive --standalone --email ${SSLMAIL} --rsa-key-size ${RSA_KEY_SIZE} -d ${MYDOMAIN} -d www.${MYDOMAIN} certonly >>/root/stderror.log 2>&1  >> /root/stdout.log
+		./letsencrypt-auto --agree-tos --renew-by-default --non-interactive --standalone --email ${SSLMAIL} --rsa-key-size ${RSA_KEY_SIZE} -d ${MYDOMAIN} -d www.${MYDOMAIN} certonly ${log} 
 	fi
 	ln -s /etc/letsencrypt/live/${MYDOMAIN}/fullchain.pem /etc/nginx/ssl/${MYDOMAIN}.pem
 	ln -s /etc/letsencrypt/live/${MYDOMAIN}/privkey.pem /etc/nginx/ssl/${MYDOMAIN}.key.pem
 else
 	echo "${info} Creating self-signed SSL certificates..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
-	openssl ecparam -genkey -name secp384r1 -out /etc/nginx/ssl/${MYDOMAIN}.key.pem >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-	openssl req -new -sha256 -key /etc/nginx/ssl/${MYDOMAIN}.key.pem -out /etc/nginx/ssl/csr.pem -subj "/C=DE/ST=Private/L=Private/O=Private/OU=Private/CN=*.${MYDOMAIN}" >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-	openssl req -x509 -days 365 -key /etc/nginx/ssl/${MYDOMAIN}.key.pem -in /etc/nginx/ssl/csr.pem -out /etc/nginx/ssl/${MYDOMAIN}.pem >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+	openssl ecparam -genkey -name secp384r1 -out /etc/nginx/ssl/${MYDOMAIN}.key.pem ${log}
+	openssl req -new -sha256 -key /etc/nginx/ssl/${MYDOMAIN}.key.pem -out /etc/nginx/ssl/csr.pem -subj "/C=DE/ST=Private/L=Private/O=Private/OU=Private/CN=*.${MYDOMAIN}" ${log}
+	openssl req -x509 -days 365 -key /etc/nginx/ssl/${MYDOMAIN}.key.pem -in /etc/nginx/ssl/csr.pem -out /etc/nginx/ssl/${MYDOMAIN}.pem ${log}
 fi
 
 HPKP1=$(openssl x509 -pubkey < /etc/nginx/ssl/${MYDOMAIN}.pem | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | base64)
 HPKP2=$(openssl rand -base64 32)
 
 echo "${info} Creating strong Diffie-Hellman parameters, please wait..." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
-openssl dhparam -out /etc/nginx/ssl/dh.pem ${RSA_KEY_SIZE} >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+openssl dhparam -out /etc/nginx/ssl/dh.pem ${RSA_KEY_SIZE} ${log}
 
 mkdir -p /etc/nginx/html/${MYDOMAIN}
 
@@ -411,10 +411,12 @@ sed -i "6s/^/\t\t\t index\t\t\t\t index.php index.html index.htm;/n" /etc/nginx/
 
 fi
 
-
-
-
-
+if [ ${HIGH_SECURITY} == '1' ]; then
+	sed -i "26d" /etc/nginx/sites-available/${MYDOMAIN}.conf
+	sed -i "26i ssl_protocols TLSv1.2;" /etc/nginx/sites-available/${MYDOMAIN}.conf
+	sed -i "33d" /etc/nginx/sites-available/${MYDOMAIN}.conf
+	sed -i "33i ssl_ciphers AES256+EECDH:AES256+EDH:!aNULL;" /etc/nginx/sites-available/${MYDOMAIN}.conf
+fi
 
 
 if [ ${USE_PHP7} == '1' ] && [ ${USE_PHP5} == '0' ]; then
@@ -429,12 +431,12 @@ fi
 ln -s /etc/nginx/sites-available/${MYDOMAIN}.conf /etc/nginx/sites-enabled/${MYDOMAIN}.conf
 
 if [ ${CLOUDFLARE} == '0' ] && [ ${USE_VALID_SSL} == '1' ]; then
-	sed -i 's/#ssl/ssl/g' /etc/nginx/sites-available/${MYDOMAIN}.conf
-	sed -i 's/#resolver/resolver/g' /etc/nginx/sites-available/${MYDOMAIN}.conf
-	sed -i 's/#add/add/g' /etc/nginx/sites-available/${MYDOMAIN}.conf
+	sed -i "s/#ssl/ssl/g" /etc/nginx/sites-available/${MYDOMAIN}.conf
+	sed -i "s/#resolver/resolver/g" /etc/nginx/sites-available/${MYDOMAIN}.conf
+	sed -i "s/#add/add/g" /etc/nginx/sites-available/${MYDOMAIN}.conf
 fi
 
-fuser -k 80/tcp >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+fuser -k 80/tcp ${log}
 service nginx start
 # Restart Nginx
 systemctl -q start nginx.service
