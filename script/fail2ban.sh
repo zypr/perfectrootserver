e37677731247beba681646b589cae29d67b00cd9
shoujii <retender.jw@gmail.com>
2017-04-16 00:34:39 +0200

    Update fail2ban.sh

--- script/fail2ban.sh
+++ script/fail2ban.sh
@@ -26,16 +26,16 @@
 #################################
 
 fail2ban() {
-mkdir -p ~/sources/${FAIL2BAN_VERSION}/ >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-cd ~/sources/${FAIL2BAN_VERSION}/ >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-wget https://github.com/fail2ban/fail2ban/archive/${FAIL2BAN_VERSION}.tar.gz >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+mkdir -p ~/sources/${FAIL2BAN_VERSION}/ ${log}
+cd ~/sources/${FAIL2BAN_VERSION}/ ${log}
+wget https://github.com/fail2ban/fail2ban/archive/${FAIL2BAN_VERSION}.tar.gz ${log}
 
-tar -xzf ${FAIL2BAN_VERSION}.tar.gz >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+tar -xzf ${FAIL2BAN_VERSION}.tar.gz ${log}
 cd fail2ban-${FAIL2BAN_VERSION}
-python setup.py -q install >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+python setup.py -q install ${log}
 
-mkdir -p /var/run/fail2ban >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-cp ~/files/fail2ban/conf/fail2ban.service /etc/systemd/system/fail2ban.service >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+mkdir -p /var/run/fail2ban ${log}
+cp ~/files/fail2ban/conf/fail2ban.service /etc/systemd/system/fail2ban.service ${log}
 [[ -f /lib/systemd/system/fail2ban.service ]] && rm /lib/systemd/system/fail2ban.service
 if [[ ! -f /var/log/mail.warn ]]; then
 	touch /var/log/mail.warn
@@ -47,9 +47,9 @@ cp ~/files/fail2ban/conf/jail.d/*.conf /etc/fail2ban/jail.d/
 #rm -rf ~/sources/files/fail2ban/inst/${FAIL2BAN_VERSION}
 [[ -z $(grep fail2ban /etc/rc.local) ]] && sed -i '/^exit 0/i\test -d /var/run/fail2ban || install -m 755 -d /var/run/fail2ban/' /etc/rc.local
 mkdir -p /var/run/fail2ban/
-service fail2ban start >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-systemctl -q daemon-reload >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
-systemctl -q enable fail2ban >>/root/logs/stderror.log 2>&1 >>/root/logs/stdout.log
+service fail2ban start ${log}
+systemctl -q daemon-reload ${log}
+systemctl -q enable fail2ban ${log}
 }
 source ~/configs/versions.cfg
 
